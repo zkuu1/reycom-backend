bf907b3ee293c7ea2f81a512a68449f3b99251f3
Zkuu <142339560+zkuu1@users.noreply.github.com>
2026-01-07 21:12:11 +0700

    feat: make errorResponse

--- src/controllers/apply/apply-controller.ts
+++ src/controllers/apply/apply-controller.ts
@@ -8,6 +8,28 @@ export const ApplyController = new Hono<ContextWithPrisma>();
 
 ApplyController.get('/apply', authAdminMiddleware, withPrisma, async (c) => {
     const prisma = c.get('prisma');
-    const response = await ApplyService.getAllApply(prisma);
+    const response = await ApplyService.GetAllApply(prisma);
     return c.json(response, 200);
 });
+
+ApplyController.post('/apply', withPrisma, async (c) => {
+    const prisma = c.get('prisma');
+    const request = await c.req.json();
+    const response = await ApplyService.CreateApply(prisma, request);
+    return c.json(response, 201);
+})
+
+ApplyController.patch('/apply/:id', authAdminMiddleware, withPrisma, async (c) => {
+    const prisma = c.get('prisma');
+    const id_apply = Number(c.req.param('id'));
+    const request = await c.req.json();
+    const response = await ApplyService.UpdateApplyById(prisma, id_apply, request);
+    return c.json(response, 200);
+})
+
+ApplyController.delete('/apply/:id', authAdminMiddleware, withPrisma, async (c) => {
+    const prisma = c.get('prisma');
+    const id_apply = Number(c.req.param('id'));
+    const response = await ApplyService.DeleteApplyById(prisma, id_apply);
+    return c.json(response, 200);
+})
--- src/services/apply/apply-service.ts
+++ src/services/apply/apply-service.ts
@@ -13,12 +13,24 @@ import { applyValidation } from '../../validations/apply/apply-validation.js';
 
 export class ApplyService {
 
-    static async createApply(
+  // ===============================
+  // CREATE APPLY
+  // ===============================
+    static async CreateApply(
         prisma: PrismaClient,
         request: CreateApplyRequest,
     ): Promise<ApiResponse<ApplyData>> {
 
         const validatedRequest = applyValidation.CREATE.parse(request);
+        const existing = await prisma.applys.findUnique({
+        where: { email: validatedRequest.email },
+        });
+
+        if (existing) {
+        throw new HTTPException(400, {
+            message: 'Apply with the same email already exists',
+        });
+        }
 
         const apply = await prisma.applys.create({
             data: validatedRequest,
@@ -27,7 +39,10 @@ export class ApplyService {
         return toApplyResponse(apply, 'Apply created successfully');
     }
 
-    static async getAllApply(
+  // ===============================
+  // GET ALL APPLY
+  // ===============================
+    static async GetAllApply(
         prisma: PrismaClient,
     ): Promise<ApiResponse<ApplyData[]>> {
 
@@ -37,5 +52,64 @@ export class ApplyService {
             applys,
             'Get all applys successfully'
         );
+    }
+
+  // ===============================
+  // UPDATE APPLY BY ID
+  // ===============================
+    static async UpdateApplyById(
+        prisma: PrismaClient,
+        id: number,
+        request: Partial<CreateApplyRequest>,
+    ): Promise<ApiResponse<ApplyData>> {
+        const validatedRequest = applyValidation.UPDATE.parse(request);
+
+        const existingApply = await prisma.applys.findUnique({
+            where: { id },
+        });
+
+        if (!existingApply) {
+            throw new HTTPException(404, { message: 'Apply not found' });
+        }
+
+        const existing = await prisma.applys.findUnique({
+        where: { email: validatedRequest.email },
+        });
+
+        if (existing) {
+        throw new HTTPException(400, {
+            message: 'Apply with the same email already exists',
+        });
+        }
+
+        const updatedApply = await prisma.applys.update({
+            where: { id },
+            data: request,
+        });
+
+        return toApplyResponse(updatedApply, 'Apply updated successfully');
+    }
+
+  // ===============================
+  // DELETE APPLY BY ID
+  // ===============================
+    static async DeleteApplyById(
+        prisma: PrismaClient,
+        id: number,
+    ): Promise<ApiResponse<ApplyData>> {
+
+        const existingApply = await prisma.applys.findUnique({
+            where: { id },
+        });
+
+        if (!existingApply) {
+            throw new HTTPException(404, { message: 'Apply not found' });
+        }
+
+        const deletedApply = await prisma.applys.delete({
+            where: { id },
+        });
+
+        return toApplyResponse(deletedApply, 'Apply deleted successfully');
     }
 }
